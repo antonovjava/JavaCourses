52926bc771a162bc2201c693e5d27c0237da69ed
antonovjava <volok10@ya.ru>
2017-04-05 21:33:56 +0400

    firstCmmt

---
+++ Calculate.java
@@ -0,0 +1,11 @@
+//05.04.2017
+
+public class Calculate{
+    public static void main(String[] arg){
+        System.out.println("Calculate...");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int sum = first + second;
+        System.out.println("Sum: " + sum);
+    }
+        }
